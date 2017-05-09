e7c639fc4866724aa38a5c2248d68895c29f18fa
Oleh Sliusar <olehsliusar@gmail.com>
2017-05-09 15:18:39 +0300

    Add #padded method

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,11 +1,15 @@
 class Timer
   attr_accessor :seconds
-  
+
   def seconds
     @seconds = 0
   end
 
   def time_string
     @time_string = Time.at(@seconds).utc.to_s.split[1]
+  end
+
+  def padded(seconds)
+    seconds.to_s.rjust(2, '0')
   end
 end
